e7985f42e69234736e0ed0c2369fc97fb4846d21
Valentin GIANNINI <valentin.giannini@telecomnancy.net>
2015-05-18 23:45:29 +0200

    modification du script pour relancer les segfault, adaptation des scripts plot

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-protocoles="AODV DSR"
+protocoles="DSR AODV"
 
 for p in $protocoles
 do
 
+	echo $p
 
 	sed  's/PROTOCOLE_VAR/'$p'/g' protocol.tcl > $p".tcl"
-
 
 	rm graph.dat 2>/dev/null
 	for s in {1..10}
@@ -20,7 +20,14 @@ do
 			echo  -n $i
 			[[ $i = 30 ]] && echo  "" || echo -n ","
 			sed  's/SOURCES_VAR/'$s'/g' $p".tcl" > tmp.tcl
-			ns tmp.tcl  &> /dev/null
+			
+			ok=1
+			while [ $ok -ne 0 ]
+			do
+				ns tmp.tcl  &> /dev/null
+				ok=$?
+			done
+
 			rm tmp.tcl
 			awk -f script.awk results.tr  | tail -n 1 >> moy.tr
 			rm results.tr 2>/dev/null
@@ -34,8 +41,8 @@ do
 
 	rm $p".tcl" 2>/dev/null
 
-	mkdir -p 'courbes/'$p 2>/dev/null
-	gnuplot < plot.options
+	mkdir -p 'courbes/' 2>/dev/null
+	gnuplot < 'plot_'$p'.options'
 	echo "courbes générées" 
 
 done
